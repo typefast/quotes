64adc6a0557b85a34fb849633642b93be1e7f95b
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-27 20:47:58 +0000

    Add acts_as_votable to Quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,6 @@
 class Quote < ActiveRecord::Base
   validates :quote, presence: true, length: {minimum: 3}
   validates :author, presence: true, length: {minimum: 3}
+  
+  acts_as_votable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127200331) do
+ActiveRecord::Schema.define(version: 20160127204552) do
 
   create_table "quotes", force: :cascade do |t|
     t.text     "quote"
@@ -19,5 +19,20 @@ ActiveRecord::Schema.define(version: 20160127200331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
