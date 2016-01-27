daf487d7bbab77723567f40f123609048e4a7974
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-27 20:05:38 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'quotes/index'
-
-  get 'quotes/new'
-
-  get 'quotes/show'
-
+  
+  resources :quotes
+  root 'quotes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
