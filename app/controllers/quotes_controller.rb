53e0ef5c5df7199879dd7f4dee4c4f325eec75a0
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-27 20:21:56 +0000

    Add quote logic to controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,35 @@
 class QuotesController < ApplicationController
   def index
+    @quotes = Quote.all
   end
 
   def new
+    @quote = Quote.find(params[:id])
   end
   
   def create
+    @quote = Quote.create(quote_params)
+    if @quote.save
+      flash[:success] = "Quote Saved"
+      redirect_to @quote
+    else
+      flash[:error] = "There was an error"
+      render :new
+    end
   end
 
   def show
+    @quote = Quote.find(params[:id])
   end
+  
+  private
+    def quote_params
+      params.require(:quote).permit(:quote, :author)
+    end
 end
+
+
+
+
+
+
